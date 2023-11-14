824380df96c30e437e172816496fa240a81cf9ab
Alicia Liu <alicia.liu04@gmail.com>
2023-11-14 00:33:52 -0800

    finish setting up

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomePage from './components/welcome-page.jsx';
+import { PaperProvider } from 'react-native-paper';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,11 +14,11 @@ export default function App() {
     <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Welcome">
-          <Stack.Screen name="Welcome" component={WelcomePage}/>
+          <Stack.Screen name="Welcome" component={WelcomePage} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
-    
+
   );
 }
 
