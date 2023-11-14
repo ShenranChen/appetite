e46e2dfa19c1c79910ba8c8067596d16d343a293
Shenran <shenran0829@gmail.com>
2023-11-14 00:00:06 -0800

    settting up project, downloaded dependencies

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomePage from './components/welcome-page.jsx';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={WelcomePage}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+    
   );
 }
 
